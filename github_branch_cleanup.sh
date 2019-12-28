a7a77b60bef9e68d4c3d8a48829b0c5ac6d4f44b
devops090 <51469018+devops090@users.noreply.github.com>
2019-12-29 03:59:25 +0530

    Update github_branch_cleanup.sh
    dkjkhfjg

--- github_branch_cleanup.sh
+++ github_branch_cleanup.sh
@@ -3,24 +3,24 @@ filename=repolist.txt
 while read -r line; do
     name="$line"
     echo "Name read from file - $name"
-	git clone https://github.com/devops090/$name.git
+    	git clone https://github.com/devops090/$name.git
+    pwd
+    cd $name
+	#git checkout master
+	#deleted merged branches on master branch
+    tarBranch=$(git branch -r --merged | grep -v master | sed 's/origin\///')
+	for branch in $tarBranch
+	do
+ 	  echo $branch
+ 	  lastDate=$(git show -s --format=%ci origin/$branch)
+          convertDate=$(echo $lastDate | cut -d' ' -f 1)
+ 	  Todate=$(date -d "$convertDate" +'%s')
+          current=$(date +'%s')
+ 	  day=$(( ( $current - $Todate )/60/60/24 ))
+          echo "last commit on $branch branch was $day days ago"
+ 	 	if [ "$day" -gt 90 ]; then
+    	   	   echo git push origin :$branch
+    		   echo "delete the old branch $branch"
+ 		fi
+	done
 done < "$filename"
-pwd
-cd $name
-git checkout master
-#deleted merged branches on master branch
-tarBranch=$(git branch -r --merged | grep -v master | sed 's/origin\///')
-for branch in $tarBranch
-do
- echo $branch
- lastDate=$(git show -s --format=%ci origin/$branch)
- convertDate=$(echo $lastDate | cut -d' ' -f 1)
- Todate=$(date -d "$convertDate" +'%s')
- current=$(date +'%s')
- day=$(( ( $current - $Todate )/60/60/24 ))
- echo "last commit on $branch branch was $day days ago"
- if [ "$day" -gt 90 ]; then
-    echo git push origin :$branch
-    echo "delete the old branch $branch"
- fi
-done
