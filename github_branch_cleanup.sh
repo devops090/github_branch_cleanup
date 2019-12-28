9698679bdeaa8e7f94ccf1d59e50b72508fd0f30
devops090 <51469018+devops090@users.noreply.github.com>
2019-12-29 03:53:28 +0530

    Update github_branch_cleanup.sh
    nsdjnfjk

--- github_branch_cleanup.sh
+++ github_branch_cleanup.sh
@@ -1,5 +1,5 @@
 curl -i https://api.github.com/users/devops090/repos -u devops090:nskhpm@123 | sed -e 's/[{}]/''/g' | grep "name" | sed '/name/!d' | sed s/\"name\"://g | sed s/\"//g | sed s/\,//g | sed '/devops090/d'| xargs -n1 &>> ./repolist.txt
-filename="$repolist.txt"
+filename=repolist.txt
 while read -r line; do
     name="$line"
     echo "Name read from file - $name"
