edc467b82badcdeca41dc281e947b189b0029e39
devops090 <51469018+devops090@users.noreply.github.com>
2020-01-03 20:38:12 +0530

    Create gitreport.sh
    trdtrdrd

---
+++ gitreport.sh
@@ -0,0 +1,18 @@
+filename=repolist.csv
+while read -r line; do
+    name="$line"
+      echo "Name read from file - $name"
+        curl -u devops090:12ec1e8588c3e7518f1f2671905bb7d47f055d37 -X GET https://api.github.com/repos/devops090/$name/branches | sed -e 's/[{}]/''/g' | grep "name" | sed 's/name/'"$name"'/g' | sed s/\"//g | sed s/\,//g | sed 's/:/,/g' | sed 's/ //g' | grep -v master | sed 's/origin\///' | xargs -n1 &>> ./branches_list.csv
+  done < "$filename"
+sed -e /^$/d branches_list.csv &>> ./repo_branch_list.csv
+rm -rf branches_list.csv repolist.csv
+#awk -F ',' '{print $2}' repo_branch_list.csv
+repo_branch=repo_branch_list.csv
+while IFS=',' read -r f1 f2; do
+  name_repo="$f1"
+  name_branch="$f2"
+     echo "Name read from line repo - $name_repo"
+     echo "Name read from line branch - $name_branch"
+        curl -u devops090:12ec1e8588c3e7518f1f2671905bb7d47f055d37 -X GET https://api.github.com/repos/devops090/"$name_repo"/commits/"$name_branch" | grep "date" | sed 's/date/'"$name_repo"','"$name_branch"'/g' | sed s/\"//g | sed 's/:/,/g' | sed 's/ //g' | head -n1 &>> ./lastcommit_date.csv
+  done < "$repo_branch"
+  
