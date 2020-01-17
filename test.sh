803ea28d356b18803a56d9f9f3267352eb714776
devops090 <51469018+devops090@users.noreply.github.com>
2020-01-17 23:21:27 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,20 +1,6 @@
 execfunction () { 
   arr=("$@")
   echo "${arr[@]}"
-  while IFS=',' read -r f1 f2; do
-      name_repo="$f1"
-      name_branch="$f2"
-      echo "Name read from line repo - $name_repo"
-      echo "Name read from line branch - $name_branch"
-      dateformat=$(curl -u devops090:e952d0879410a1958c31fef0dcd24e443dd9be7e  -X GET https://api.github.com/repos/devops090/"$name_repo"/commits/"$name_branch" | grep "date" | sed 's/date/'"$name_repo"','"$name_branch"'/g' | sed s/\"//g | sed 's/:/,/g' | sed 's/ //g' | head -n1)
-        convertDate=$(echo $dateformat | cut -d' ' -f 1)
-        Todate=$(date -d "$convertDate" +'%s')
-          current=$(date +'%s')
-        day=$(( ( $current - $Todate )/60/60/24 ))
-        if [ "$day" -gt 90 ]; then
-          echo ""$name_repo","$name_branch","$dateformat","$day"-days ago" &>> ./last_commit_history_of_braches.csv
-        fi
-  done < <(printf '%s\n' "${arr[@]}")
 }
 
 func () {
