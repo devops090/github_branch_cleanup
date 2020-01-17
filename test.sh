68df5fcdda0cea3358c87fe58939b68ac4652683
devops090 <51469018+devops090@users.noreply.github.com>
2020-01-17 23:14:20 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,14 +1,6 @@
-res=$(curl -i https://api.github.com/users/devops090/repos -u devops090:e952d0879410a1958c31fef0dcd24e443dd9be7e   | sed -e 's/[{}]/''/g' | grep "name" | sed '/name/!d' | sed s/\"name\"://g | sed s/\"//g | sed s/\,//g | sed '/devops090/d'| xargs -n1)
-
-while read -r line; do
-    name="$line"
-    echo "Name read from file - $name"
-    branch=$(curl -u devops090:e952d0879410a1958c31fef0dcd24e443dd9be7e  -X GET https://api.github.com/repos/devops090/$name/branches | sed -e 's/[{}]/''/g' | grep "name" | sed 's/name/'"$name"'/g' | sed s/\"//g | sed s/\,//g | sed 's/:/,/g' | sed 's/ //g' | grep -v master | sed 's/origin\///' | xargs -n1)
-    execfunction "${branch[@]}" &
-done < <(printf '%s\n' "$res")
-
 execfunction(){ 
   arr=("$@")
+  echo ${arr[@]}
   while IFS=',' read -r f1 f2; do
       name_repo="$f1"
       name_branch="$f2"
@@ -24,3 +16,15 @@ execfunction(){
         fi
   done < <(printf '%s\n' "${arr[@]}")
 }
+
+func(){ 
+  res=$(curl -i https://api.github.com/users/devops090/repos -u devops090:e952d0879410a1958c31fef0dcd24e443dd9be7e   | sed -e 's/[{}]/''/g' | grep "name" | sed '/name/!d' | sed s/\"name\"://g | sed s/\"//g | sed s/\,//g | sed '/devops090/d'| xargs -n1)
+  while read -r line; do
+      name="$line"
+      echo "Name read from file - $name"
+      branch=$(curl -u devops090:e952d0879410a1958c31fef0dcd24e443dd9be7e  -X GET https://api.github.com/repos/devops090/$name/branches | sed -e 's/[{}]/''/g' | grep "name" | sed 's/name/'"$name"'/g' | sed s/\"//g | sed s/\,//g | sed 's/:/,/g' | sed 's/ //g' | grep -v master | sed 's/origin\///' | xargs -n1)
+      execfunction "${branch[@]}" &
+  done < <(printf '%s\n' "$res")
+}
+
+func
